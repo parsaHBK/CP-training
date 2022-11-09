20e5d77b7bbfaf11498c60cb1f6904d3b500cab6
Parsa Abyar <117229743+parsaHBK@users.noreply.github.com>
2022-11-09 10:09:21 +0330

    Update T2.cpp

--- T2.cpp
+++ T2.cpp
@@ -9,22 +9,22 @@ char sign;
 cout<<"guide!!you can use experssion with this charcters(*,-,+,/)"<<"\nenter one experssion: ";
 cin>>a>>sign>>b;
 
-if ('sub')
+if (sign=='+')
 {
 	sum=a+b;
 		cout<<sum;
 }
-else if (a-b)
+else if (sign=='-')
 {
 	sub=a-b;
 		cout<<sub;
 }
-else if (a*b)
+else if (sign=='*')
 {
 	mul=a*b;
 		cout<<mul;
 }
-else if (a/b)
+else if (sign=='/')
 {
 	div=a/b;
 		cout<<div;
